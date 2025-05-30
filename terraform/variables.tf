f3ca07d2ffa0ea6525de40305478a39e3967b8b8
Belal Waw <belalwaw@Belals-MacBook-Pro.local>
2025-05-30 16:31:55 +0100

    fixing tf error

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,12 +2,3 @@ variable "do_token" {}
 variable "github_repo" {
   description = "GitHub repository in format user/repo; supplied via TF_VAR_github_repo"
 }
-variable "spaces_access_key" {
-  type        = string
-  description = "DigitalOcean Spaces access key (for remote state)"
-}
-
-variable "spaces_secret_key" {
-  type        = string
-  description = "DigitalOcean Spaces secret key (for remote state)"
-}
