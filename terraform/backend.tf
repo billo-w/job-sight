8bb1bb00fa7588041f161337b8318cbc064c20a0
Belal Waw <Belal.Waw@morson.com>
2025-06-03 16:10:06 +0100

    made backend github actions compatible

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,10 +4,8 @@ terraform {
     bucket                      = "job-sight-terraform"
     key                         = "terraform.tfstate"
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_s3_checksum            = true
     region                      = "us-east-1"
   }
 }
