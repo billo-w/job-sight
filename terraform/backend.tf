66af65a2c00e09f2b3240f541fc8fe0ec7e4d4fc
Belal Waw <belalwaw@Belals-MacBook-Pro.local>
2025-06-15 06:03:32 +0100

    added tf validate

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,8 +7,8 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_s3_checksum            = true   # ← now supported
-    skip_requesting_account_id  = true   # ← now supported
+    skip_s3_checksum            = true 
+    skip_requesting_account_id  = true 
     region                      = "us-east-1"
   }
 }
